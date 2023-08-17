ff0433c6931e603899d8d887a0f3d6cd9b2f9f99
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-08-17 12:57:39 -0600

    Playing with resquest object and data trasnferred from and to api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,17 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "this is your Django API response"})
+    body = request.body #byte string of JSON data
+    print(request.GET)
+    print(body)
+    data = {}
+    try: 
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- client/basic.py
+++ client/basic.py
@@ -4,8 +4,8 @@ import requests
 # endopoint = "https://httpbin.org/anything"
 endopoint = "http://localhost:8000/api"
 
-get_response = requests.get(endopoint, data={"query":"hello world"})
-print(get_response.text)
+get_response = requests.get(endopoint, params={"abc": 123}, json={"query":"hello world"})
+# print(get_response.text)
 print(get_response.json())
 print(get_response.status_code)
-print(get_response.json()["message"])
+# print(get_response.json()["message"])
