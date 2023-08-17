848d77e166835478503dc52d88b7b10db3a03516
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-08-17 12:03:08 -0600

    adding a new /api/ endpoing and a server response

--- client/basic.py
+++ client/basic.py
@@ -2,9 +2,10 @@ import requests
 
 # endopoint = "https://httpbin.org/status/200"
 # endopoint = "https://httpbin.org/anything"
-endopoint = "http://localhost:8000"
+endopoint = "http://localhost:8000/api"
 
 get_response = requests.get(endopoint, data={"query":"hello world"})
 print(get_response.text)
-# print(get_response.json())
+print(get_response.json())
 print(get_response.status_code)
+print(get_response.json()["message"])
